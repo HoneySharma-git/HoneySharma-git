431a5bc27b415fbbae519fd5da5e558373242704
HoneySharma-git <honeyambardar@gmail.com>
2025-05-08 14:02:52 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+###Task 1 ###
+
+a= int(input('Enter First Number and Press Enter:'))
+
+#a=int(input())
+
+b= int(input('Enter Second Number and Press Enter:'))
+c=a+b
+d=a-b
+e=a*b
+f=a/b
+print("Addition of First and Second No. is: ",c)
+print("Substraction of First and Second No. is: ",d)
+print("Multiplication of First and Second No. is: ",e)
+print("Division of First and Second No. is: ",f)
+
+
---
+++ Tast 2.py
@@ -0,0 +1,10 @@
+###Task 2 ###
+
+a=  input('Enter Your First Name:')
+
+b=  input('Enter Your Last NAMe and Press Enter:')
+c=a+" " +b +"!"+" " + "Welcome to the Python Program"
+
+print("Hello", c )
+
+
